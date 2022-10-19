361c20b5a52b913baa6b97d33652d173593753c3
makeevrserg <makeevrserg@gmail.com>
2022-10-19 21:58:10 +0300

    refactor: Fixed dependencies in shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.compose.compose
 
 plugins {
     kotlin("plugin.serialization")
-    id("org.jetbrains.compose")
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
@@ -33,21 +32,13 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                // Compose
-                api(compose.runtime)
-                api(compose.foundation)
-                api(compose.material)
                 // Ktor
                 implementation("io.ktor:ktor-client-core:${Dependencies.Domain.ktor}")
                 implementation("io.ktor:ktor-client-serialization:${Dependencies.Domain.ktor}")
                 implementation("io.ktor:ktor-client-json:${Dependencies.Domain.ktor}")
                 implementation("io.ktor:ktor-client-content-negotiation:${Dependencies.Domain.ktor}")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:${Dependencies.Domain.ktor}")
-                // Serializtion
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Dependencies.Kotlin.serializationJson}")
-                // Navigation
-                implementation("com.arkivanov.decompose:decompose:${Dependencies.Kotlin.decompose}")
-                implementation("com.arkivanov.decompose:extensions-compose-jetbrains:${Dependencies.Kotlin.decompose}")
             }
         }
         val commonTest by getting {
@@ -56,28 +47,8 @@ kotlin {
             }
         }
 
-        val desktopMain by getting {
-            dependencies {
-                api(compose.preview)
-                implementation("io.ktor:ktor-client-okhttp:${Dependencies.Domain.ktor}")
-                implementation("io.ktor:ktor-client-core:${Dependencies.Domain.ktor}")
-//                implementation("org.threeten:threetenbp:1.6.0")
-            }
-        }
-
-        val androidMain by getting {
-            dependencies {
-                implementation(compose.foundation)
-                implementation(compose.ui)
-                implementation(compose.material)
-                implementation(compose.materialIconsExtended)
-                implementation(compose.runtime)
-                implementation(compose.preview)
-                implementation(compose.uiTooling)
-                implementation("io.ktor:ktor-client-okhttp:${Dependencies.Domain.ktor}")
-                implementation("io.ktor:ktor-client-core:${Dependencies.Domain.ktor}")
-            }
-        }
+        val desktopMain by getting
+        val androidMain by getting
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -110,16 +81,3 @@ android {
         targetSdk = 33
     }
 }
-dependencies {
-    implementation("androidx.compose.ui:ui-graphics:1.2.1")
-}
-
-
-//dependencies {
-//    add("kspCommonMainMetadata", "de.jensklingenberg.ktorfit:ktorfit-ksp:${Dependencies.Kotlin.ktorfit}")
-//    add("kspAndroid", "de.jensklingenberg.ktorfit:ktorfit-ksp:${Dependencies.Kotlin.ktorfit}")
-//    add("kspIosX64", "de.jensklingenberg.ktorfit:ktorfit-ksp:${Dependencies.Kotlin.ktorfit}")
-//    add("kspIosSimulatorArm64", "de.jensklingenberg.ktorfit:ktorfit-ksp:${Dependencies.Kotlin.ktorfit}")
-//    add("kspJvm", "de.jensklingenberg.ktorfit:ktorfit-ksp:${Dependencies.Kotlin.ktorfit}")
-//
-//}
